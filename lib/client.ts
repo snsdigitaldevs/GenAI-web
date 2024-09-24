27a3baee8fb8c3ade8b2f9b99ec0c715307e3346
guangzhengli <guangzheng.li@thoughtworks.com>
2024-09-24 20:11:37 +0800

    feat: try to fix amplify_outputs.json not found deploy issue

--- lib/client.ts
+++ lib/client.ts
@@ -1,6 +1,6 @@
 "use client"
 
-import type { Schema } from '@amplify/data/resource'
+import type { Schema } from '@/amplify/data/resource'
 import { generateClient } from 'aws-amplify/data'
 import { Amplify } from 'aws-amplify';
 import outputs from "@/amplify_outputs.json";
