4a68de420dcc43c14a9dc567161ce7306b184831
guangzhengli <guangzheng.li@thoughtworks.com>
2024-09-24 18:17:52 +0800

    feat: fix Amplify.configure outputs file

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -2,9 +2,9 @@
 
 import type { Schema } from '@amplify/data/resource'
 import { generateClient } from 'aws-amplify/data'
-// import { Amplify } from 'aws-amplify';
-// import outputs from '../../amplify_outputs.json';
-//
-// Amplify.configure(outputs);
+import { Amplify } from 'aws-amplify';
+import outputs from "@/amplify_outputs.json";
+
+Amplify.configure(outputs);
 
 export const client = generateClient<Schema>()
