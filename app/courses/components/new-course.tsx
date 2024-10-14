da92f57dae01006c0ba9907ece8c9bb4ce295a07
guangzhengli <guangzheng.li@thoughtworks.com>
2024-10-14 17:19:33 +0800

    feat: add multi step loader to create new course

--- app/courses/components/new-course.tsx
+++ app/courses/components/new-course.tsx
@@ -28,6 +28,7 @@ import { PlusCircledIcon } from '@radix-ui/react-icons'
 import { createScript, generateStructuresAndVocabulary, createCourse } from "../actions"
 import { useRouter } from "next/navigation"
 import { Course } from "@/lib/course/types"
+import { MultiStepLoader } from "@/components/ui/multi-step-loader"
 
 const FormSchema = z.object({
   originLanguage: z.string().min(2, {
@@ -40,6 +41,21 @@ const FormSchema = z.object({
   prompt: z.string().optional(),
 })
 
+const loadingStates = [
+  { text: "Analyzing language patterns" },
+  { text: "Generating vocabulary list" },
+  { text: "Crafting grammar structures" },
+  { text: "Creating cultural context" },
+  { text: "Optimizing learning sequence" },
+  { text: "Personalizing course content" },
+  { text: "Finalizing course structure" },
+  { text: "Generating Lesson 1" },
+  { text: "Generating Lesson 2" },
+  { text: "Generating Lesson 3" },
+  { text: "Generating Lesson 4" },
+  { text: "Generating Lesson 5" },
+];
+
 export function NewCourse() {
   const [loading, setLoading] = useState(false)
   const [isDialogOpen, setIsDialogOpen] = useState(false)
@@ -49,7 +65,7 @@ export function NewCourse() {
     resolver: zodResolver(FormSchema),
     defaultValues: {
       originLanguage: "English",
-      targetLanguage: "Chinese", 
+      targetLanguage: "Chinese",
     },
   })
 
@@ -58,9 +74,10 @@ export function NewCourse() {
     console.info(`Submitting task: ${data.originLanguage} -> ${data.targetLanguage}`)
 
     try {
+      setIsDialogOpen(false)
       const structureVocabulary = await generateStructuresAndVocabulary(data.targetLanguage)
       console.info(`Structure Vocabulary: ${structureVocabulary}`)
-  
+
       const course = await createCourse({
         origin: data.originLanguage,
         target: data.targetLanguage,
@@ -77,7 +94,6 @@ export function NewCourse() {
       await createScript(course.data.id, structureVocabulary)
       router.push(`/courses/${course.data?.id}`);
       setLoading(false)
-      setIsDialogOpen(false)
     } catch (error) {
       console.error(`Error submitting task: ${error}`)
       setLoading(false)
@@ -86,100 +102,104 @@ export function NewCourse() {
   }
 
   return (
-    <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
-      <DialogTrigger asChild>
-        <Button onClick={() => setIsDialogOpen(true)}>
-          <PlusCircledIcon className="size-4 mr-2" />
-          Add Course
-        </Button>
-      </DialogTrigger>
-      <DialogContent className="sm:max-w-4xl">
-        <DialogHeader>
-          <DialogTitle>Add New Course</DialogTitle>
-          <DialogDescription>
-            Add a new course to your list.
-          </DialogDescription>
-        </DialogHeader>
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-6">
-            <FormField
-              control={form.control}
-              name="originLanguage"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Origin Language</FormLabel>
-                  <FormControl>
-                    <Input placeholder="shadcn" {...field} />
-                  </FormControl>
-                  <FormDescription>
-                    This is your origin language.
-                  </FormDescription>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="targetLanguage"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Target Language</FormLabel>
-                  <FormControl>
-                    <Input placeholder="shadcn" {...field} />
-                  </FormControl>
-                  <FormDescription>
-                    This is your target language.
-                  </FormDescription>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="prompt"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Prompt</FormLabel>
-                  <FormControl>
-                    <Textarea
+    <>
+      <MultiStepLoader loadingStates={loadingStates} loading={loading} duration={5000} />
+
+      <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+        <DialogTrigger asChild>
+          <Button onClick={() => setIsDialogOpen(true)}>
+            <PlusCircledIcon className="size-4 mr-2" />
+            Add Course
+          </Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-4xl">
+          <DialogHeader>
+            <DialogTitle>Add New Course</DialogTitle>
+            <DialogDescription>
+              Add a new course to your list.
+            </DialogDescription>
+          </DialogHeader>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-6">
+              <FormField
+                control={form.control}
+                name="originLanguage"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Origin Language</FormLabel>
+                    <FormControl>
+                      <Input placeholder="shadcn" {...field} />
+                    </FormControl>
+                    <FormDescription>
+                      This is your origin language.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="targetLanguage"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Target Language</FormLabel>
+                    <FormControl>
+                      <Input placeholder="shadcn" {...field} />
+                    </FormControl>
+                    <FormDescription>
+                      This is your target language.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="prompt"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Prompt</FormLabel>
+                    <FormControl>
+                      <Textarea
                         placeholder="Optional"
                         className="resize-none"
                         {...field}
                       />
-                  </FormControl>
-                  <FormDescription>
-                    This is your prompt.
-                  </FormDescription>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="description"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Description</FormLabel>
-                  <FormControl>
-                    <Textarea
-                      placeholder="Optional"
-                      className="resize-none"
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormDescription>
-                    This is your description.
-                  </FormDescription>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <Button type="submit" disabled={loading}>
-              {loading ? "Submitting..." : "Submit"}
-            </Button>
-          </form>
-        </Form>
-      </DialogContent>
-    </Dialog>
+                    </FormControl>
+                    <FormDescription>
+                      This is your prompt.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="description"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Description</FormLabel>
+                    <FormControl>
+                      <Textarea
+                        placeholder="Optional"
+                        className="resize-none"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormDescription>
+                      This is your description.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit" disabled={loading}>
+                {loading ? "Submitting..." : "Submit"}
+              </Button>
+            </form>
+          </Form>
+        </DialogContent>
+      </Dialog>
+    </>
   )
 }
