ec22929cf60add4ddc9a21232f0c311281969b44
NiFang <ni.fang@thoughtworks.com>
2024-10-15 14:18:29 +0800

    refactor: refactor CourseInfo component

--- app/courses/components/course-info.tsx
+++ app/courses/components/course-info.tsx
@@ -2,7 +2,19 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import {Course} from "@/lib/course/types";
 import TitleText from "./title-text";
 
+type CourseInfoItem = {
+  label: string;
+  value: string;
+};
+
 export default function CourseInfo({ course }: { course: Course }) {
+  const courseInfoItems = (course: Course): CourseInfoItem[] => [
+    { label: "Origin Language", value: course.origin },
+    { label: "Target Language", value: course.target },
+    { label: "Description", value: course.description || "" },
+    { label: "Prompt", value: course.prompt || "" },
+  ];
+
   return (
     <Card>
       <CardHeader>
@@ -11,23 +23,13 @@ export default function CourseInfo({ course }: { course: Course }) {
         </CardTitle>
       </CardHeader>
       <CardContent>
-        <dl className="space-y-2">
-          <div>
-            <dt className="font-semibold">Origin Language</dt>
-            <dd>{course.origin}</dd>
-          </div>
-          <div>
-            <dt className="font-semibold">Target Language</dt>
-            <dd>{course.target}</dd>
-          </div>
-          <div>
-            <dt className="font-semibold">Description</dt>
-            <dd>{course.description}</dd>
-          </div>
-          <div>
-            <dt className="font-semibold">Prompt</dt>
-            <dd>{course.prompt}</dd>
-          </div>
+        <dl className="space-y-4">
+          {courseInfoItems(course).map((item, index) => (
+            <div key={index}>
+              <dt className="font-medium">{item.label}</dt>
+              <dd className="text-[#71717A] font-normal">{item.value}</dd>
+            </div>
+          ))}
         </dl>
       </CardContent>
     </Card>
