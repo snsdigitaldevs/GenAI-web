510598ee9e8f4b335f14d8f48335b224fa91a0b9
NiFang <ni.fang@thoughtworks.com>
2024-10-15 11:08:32 +0800

    feat: extract TitleText component

---
+++ app/courses/components/title-text.tsx
@@ -0,0 +1,7 @@
+export default function TitleText({ title }: { title: string }) {
+  return (
+    <div>
+      <h3 className="text-2xl font-bold tracking-tight">{title}</h3>
+    </div>
+  )
+}
--- app/courses/page.tsx
+++ app/courses/page.tsx
@@ -3,6 +3,7 @@ import { getCourses } from "./actions"
 import { columns } from "./components/columns"
 import { DataTable } from "./components/data-table"
 import { NewCourse } from "./components/new-course"
+import TitleText from "./components/title-text"
 
 export default async function CoursePage() {
   const courses = await getCourses()
@@ -11,14 +12,14 @@ export default async function CoursePage() {
   return (
     <>
       <div className="hidden h-full flex-1 flex-col space-y-8 md:flex">
-        <div className="flex items-center justify-between space-y-2">
+        <div className="flex items-center justify-between">
           <div>
-            <h2 className="text-2xl font-bold tracking-tight">Welcome back!</h2>
+            <TitleText title="Courses" />
             <p className="text-muted-foreground">
               Here&apos;s a list of your courses!
             </p>
           </div>
-          <div className="flex items-center space-x-2">
+          <div className="flex items-center space-x-2 m-0">
             <NewCourse />
           </div>
         </div>
