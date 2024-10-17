27a7fda696a396e0e490ffd6e4761f47b81973d2
NiFang <ni.fang@thoughtworks.com>
2024-10-17 15:12:31 +0800

    feat: add vocabulary recall frequency for history lessons

--- app/courses/components/vocabulary-recall-frenquency.tsx
+++ app/courses/components/vocabulary-recall-frenquency.tsx
@@ -1,6 +1,7 @@
 import { Course, LanguageUnit, Script } from "@/lib/course/types";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 interface VocabularyRecallFrequencyProps {  
   script: Script
@@ -9,47 +10,69 @@ interface VocabularyRecallFrequencyProps {
 
 function countTargetOccurrences(text: string, target: string): number {
   const regex = new RegExp(target, 'gi');
-  const matches = text.match(regex);
+  const matches = text?.match(regex);
   return matches ? matches.length : 0;
 }
 
 export default function VocabularyRecallFrequency({ script, course }: VocabularyRecallFrequencyProps) {
-  const structureVocabulary: LanguageUnit[] = JSON.parse(course.structure_vocabulary || "");
-  const vocabularyRecallFrequencies = structureVocabulary[0].vocabulary.map((vocabularyPair) => {
-    return {
-      unit: script.lessonId,
+  const historyLanguageUnits: LanguageUnit[] = JSON.parse(course.structure_vocabulary || "")
+    .sort((a: LanguageUnit, b: LanguageUnit) => a.unit - b.unit)
+    .filter(({ unit }: LanguageUnit) => unit <= script.lessonId);
+  
+  const historyUnitVocabularyRecallFrequencyList = historyLanguageUnits.map((historyLanguageUnit) => ({
+    lesson: "Lesson " + historyLanguageUnit.unit,
+    vocabularyRecallFrequency: historyLanguageUnit.vocabulary.map((vocabularyPair) => ({
+      unit: historyLanguageUnit.unit,
       origin: vocabularyPair.origin,
       target: vocabularyPair.target,
       recallFrequency: countTargetOccurrences(script.text, vocabularyPair.target)
-    }
-  });
+    }))
+  }));
   
   return (
     <Card>
       <CardHeader className="flex flex-row justify-between space-y-0">
         <CardTitle className="text-2xl font-bold">Vocabulary Recall Frequency</CardTitle>
       </CardHeader>
-      <CardContent className="max-h-[600px] overflow-y-auto">
-        <Table>
-          <TableHeader className="bg-[#F8FAFC] h-14">
-            <TableRow>
-              <TableHead>Lesson</TableHead>
-              <TableHead>{course.origin.toUpperCase()}</TableHead>
-              <TableHead>{course.target.toUpperCase()}</TableHead>
-              <TableHead>Recall frequency</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody className="border-b-[1px] border-[#E2E8F0]">
-            {vocabularyRecallFrequencies.map((vocabularyRecallFrequency) => (
-              <TableRow key={vocabularyRecallFrequency.unit}>
-                <TableCell>{vocabularyRecallFrequency.unit}</TableCell>
-                <TableCell>{vocabularyRecallFrequency.origin}</TableCell>
-                <TableCell>{vocabularyRecallFrequency.target}</TableCell>
-                <TableCell>{vocabularyRecallFrequency.recallFrequency}</TableCell>
-              </TableRow>
+      <CardContent>
+        <Tabs defaultValue={historyUnitVocabularyRecallFrequencyList[0].lesson} className="w-full">
+          <TabsList>
+            {historyUnitVocabularyRecallFrequencyList.map((historyUnitVocabularyRecallFrequency) => (
+              <TabsTrigger value={historyUnitVocabularyRecallFrequency.lesson}>
+                {historyUnitVocabularyRecallFrequency.lesson}
+              </TabsTrigger>
             ))}
-          </TableBody>
-        </Table>
+          </TabsList>
+          {historyUnitVocabularyRecallFrequencyList.map((historyUnitVocabularyRecallFrequency) => (
+            <TabsContent 
+              value={historyUnitVocabularyRecallFrequency.lesson} 
+              key={historyUnitVocabularyRecallFrequency.lesson} 
+              className="max-h-[600px] overflow-y-auto"
+            >
+              <Table>
+                <TableHeader className="bg-[#F8FAFC] h-14">
+                  <TableRow>
+                    <TableHead className="w-[50px]">Lesson</TableHead>
+                    <TableHead className="w-[180px]">{course.origin.toUpperCase()}</TableHead>
+                    <TableHead className="w-[180px]">{course.target.toUpperCase()}</TableHead>
+                    <TableHead className="w-[50px]">Recall frequency</TableHead>
+                  </TableRow>
+                </TableHeader>
+                <TableBody className="border-b-[1px] border-[#E2E8F0]">
+                  {historyUnitVocabularyRecallFrequency.vocabularyRecallFrequency.map(
+                    ({ unit, origin, target, recallFrequency }) => (
+                    <TableRow key={unit}>
+                      <TableCell className="w-[50px]">{unit}</TableCell>
+                      <TableCell className="w-[180px]">{origin}</TableCell>
+                      <TableCell className="w-[180px]">{target}</TableCell>
+                      <TableCell className="w-[50px]">{recallFrequency}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TabsContent>
+          ))}
+        </Tabs>
       </CardContent>
     </Card>
   )
